87d60b6128e0192cbbce258789baf19809e9a3bb
Vladimir Bojinov <vladimirbojinov05@gmail.com>
2023-05-23 12:39:05 +0300

    Uploading Project

---
+++ GuessTheNumber/GuessTheNumber/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace GuessTheNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int randomNumber = random.Next(1, 101);
+            int attempts = 0;
+
+            Console.Write("Guess The number from 1 - 100: ");
+            int playerInput = int.Parse(Console.ReadLine());
+
+            while (playerInput != randomNumber)
+            {
+                if (playerInput > randomNumber)
+                {
+                    Console.WriteLine("Too Hight");
+                }
+                else
+                {
+                    Console.WriteLine("Too Low");
+                }
+
+                Console.Write("Guess The number from 1 - 100: ");
+                playerInput = int.Parse(Console.ReadLine());
+                attempts++;
+            }
+
+            if (playerInput == randomNumber)
+            {
+                Console.WriteLine("Good job you guessed the number !");
+                Console.WriteLine($"Number of attempts {attempts}");
+            }
+        }
+    }
+}
